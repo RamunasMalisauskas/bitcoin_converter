9bfe34028f9736b7f59c06b043a7bea25805603d
Ramunas <zamrif@gmail.com>
2021-01-12 14:39:59 +0200

    api fetch and inpu created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,30 @@
 import React, { useEffect, useState } from "react";
 
-function Home() {}
+function Home() {
+  const [inputData, setInputData] = useState(0);
+  const [apiData, setApiData] = useState();
+  const [display, setDisplay] = useState({ one: true, two: true, three: true });
+
+  useEffect(() => {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.bpi);
+        setApiData(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <input
+        onChange={(e) => setInputData(e.target.value)}
+        type="number"
+        name="btc"
+        id="btc"
+        placeholder="enter bitcoin"
+      />
+    </>
+  );
+}
 
 export default Home;
