bbdb7c51c177e9353fc58fb58790a1eec1d4dce1
Ramunas <zamrif@gmail.com>
2021-01-13 20:17:09 +0200

    display block styledd/added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { ConCalc } from "../../components";
+import * as S from "./Home.styled";
 
 function Home() {
   // link is saved at .env  file as environmental variable
-  return <ConCalc apiLink={`${process.env.REACT_APP_CON_CALC_API}`} />;
+  return (
+    <S.Block>
+      <ConCalc apiLink={`${process.env.REACT_APP_CON_CALC_API}`} />
+    </S.Block>
+  );
 }
 
 export default Home;
---
+++ src/pages/Home/Home.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Block = styled.div`
+  max-width: 350px;
+  padding: 40px;
+`;
