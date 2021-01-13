631d59d405360ee5c2d18675316880b39b91e96f
Ramunas <zamrif@gmail.com>
2021-01-13 15:20:20 +0200

    comments

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 function Home() {
+  /* First hook is used to set input data (bitcoin input) and use it in conversion.
+  Initial value to display is set to zero. */
   const [inputData, setInputData] = useState(0);
+  // Second hook is used su save and use data from API
   const [apiData, setApiData] = useState();
+  // Last hook is used as a display porperty for currency blocks and dropdown bar
   const [display, setDisplay] = useState({ one: true, two: true, three: true });
 
+  // useEffect is used to get data from API in first lifecicles first phase (constructor)
   useEffect(() => {
-    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    // link is saved at .env  file as environmental variable
+    fetch(`${process.env.REACT_APP_CON_CALC_API}`)
       .then((res) => res.json())
       .then((data) => {
         setApiData(data);
 
+        /* After the data has been converted into json format and saved into second hook ->
+        -> interval is set to update the date every 1 min (60000 miliseconds) */
         setInterval(() => setApiData(data), 60000);
       });
   }, []);
@@ -25,8 +33,12 @@ function Home() {
         placeholder="enter bitcoin"
       />
 
+      {/* if any of the display hooks properies has been changed - the dropdown bar becomes visible
+       with coresponding currency as the selecion option in the bar */}
       {(!display.one || !display.two || !display.three) && (
         <select
+          /* display hook is set by spreading (...) display array and updating 
+          it with selected option value as true (to be displayed) */
           onChange={(e) => setDisplay({ ...display, [e.target.value]: true })}
         >
           <option value="">add currency</option>
@@ -41,12 +53,14 @@ function Home() {
         </select>
       )}
 
+      {/* corrency converter is becomes visible only if the data from API is set */}
       {apiData && (
         <>
           {display.one && (
             <>
               <div>{apiData.bpi.USD.code}</div>
               <div>
+                {/* the converted result is formated to currency string */}
                 {parseInt(
                   apiData.bpi.USD.rate_float * inputData,
                   10
@@ -55,6 +69,7 @@ function Home() {
                   currency: apiData.bpi.USD.code,
                 })}
               </div>
+              {/* display array spread is nessesery to update individual propery not to overwrite entire array */}
               <button onClick={() => setDisplay({ ...display, one: false })}>
                 X
               </button>
@@ -98,6 +113,9 @@ function Home() {
           )}
         </>
       )}
+
+      {/* if there is no API data error message is displayed */}
+      {!apiData && <div>there has been error with conversion data</div>}
     </>
   );
 }
