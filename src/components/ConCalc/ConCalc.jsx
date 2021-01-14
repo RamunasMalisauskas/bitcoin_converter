a2dc4195ae9c1c9dc69ca269f7da18ed6574422b
Ramunas <zamrif@gmail.com>
2021-01-14 07:18:17 +0200

    comments

--- src/components/ConCalc/ConCalc.jsx
+++ src/components/ConCalc/ConCalc.jsx
@@ -19,8 +19,8 @@ function ConCalc({ apiLink }) {
       .then((data) => {
         setApiData(data);
 
-        /* After the data has been converted into json format and saved into second hook ->
-        -> interval is set to update the date every 1 min (60000 miliseconds) */
+        // After the data has been converted into json format and saved into second hook ->
+        // -> interval is set to update the date every 1 min (60000 miliseconds)
         setInterval(() => setApiData(data), 60000);
       });
   }, [apiLink]);
@@ -39,97 +39,110 @@ function ConCalc({ apiLink }) {
         placeholder="enter amount of bitcoin"
       />
 
-      {/* if any of the display hooks properies has been changed - the dropdown bar becomes visible
-       with coresponding currency as the selecion option in the bar */}
-      {(!display.one || !display.two || !display.three) && (
-        <S.SelectBlock>
-          <S.Select
-            /* display hook is set by spreading (...) display array and updating 
-          it with selected option value as true (to be displayed) */
-            onChange={(e) => setDisplay({ ...display, [e.target.value]: true })}
-          >
-            <option value="">add currency</option>
+      {
+        // if any of the display hooks properies has been changed - the dropdown bar becomes visible
+        // with coresponding currency as the selecion option in the bar
+        (!display.one || !display.two || !display.three) && (
+          <S.SelectBlock>
+            <S.Select
+              // display hook is set by spreading (...) display array and updating
+              // it with selected option value as true (to be displayed)
+              onChange={(e) =>
+                setDisplay({ ...display, [e.target.value]: true })
+              }
+            >
+              <option value="">add currency</option>
 
-            {!display.one && (
-              <option value="one">{apiData.bpi.USD.code}</option>
+              {!display.one && (
+                <option value="one">{apiData.bpi.USD.code}</option>
+              )}
+
+              {!display.two && (
+                <option value="two">{apiData.bpi.GBP.code}</option>
+              )}
+
+              {!display.three && (
+                <option value="three">{apiData.bpi.EUR.code}</option>
+              )}
+            </S.Select>
+          </S.SelectBlock>
+        )
+      }
+
+      {
+        // corrency converter is becomes visible only if the data from API is set
+        apiData && (
+          <>
+            {display.one && (
+              <S.CurrencyBlock>
+                <S.Subitle>{apiData.bpi.USD.code}</S.Subitle>
+                <S.ColorTitle>
+                  {
+                    // the converted result is formated to currency string
+                    parseInt(
+                      apiData.bpi.USD.rate_float * inputData,
+                      10
+                    ).toLocaleString("en-US", {
+                      style: "currency",
+                      currency: apiData.bpi.USD.code,
+                    })
+                  }
+                </S.ColorTitle>
+                <S.Button
+                  // display array spread is nessesery to update individual propery not to overwrite entire array
+                  onClick={() => setDisplay({ ...display, one: false })}
+                >
+                  X
+                </S.Button>
+              </S.CurrencyBlock>
             )}
 
-            {!display.two && (
-              <option value="two">{apiData.bpi.GBP.code}</option>
+            {display.two && (
+              <S.CurrencyBlock>
+                <S.Subitle>{apiData.bpi.GBP.code}</S.Subitle>
+                <S.ColorTitle>
+                  {parseInt(
+                    apiData.bpi.GBP.rate_float * inputData,
+                    10
+                  ).toLocaleString("en-US", {
+                    style: "currency",
+                    currency: apiData.bpi.GBP.code,
+                  })}
+                </S.ColorTitle>
+                <S.Button
+                  onClick={() => setDisplay({ ...display, two: false })}
+                >
+                  X
+                </S.Button>
+              </S.CurrencyBlock>
             )}
 
-            {!display.three && (
-              <option value="three">{apiData.bpi.EUR.code}</option>
+            {display.three && (
+              <S.CurrencyBlock>
+                <S.Subitle>{apiData.bpi.EUR.code}</S.Subitle>
+                <S.ColorTitle>
+                  {parseInt(
+                    apiData.bpi.EUR.rate_float * inputData,
+                    10
+                  ).toLocaleString("en-US", {
+                    style: "currency",
+                    currency: apiData.bpi.EUR.code,
+                  })}
+                </S.ColorTitle>
+                <S.Button
+                  onClick={() => setDisplay({ ...display, three: false })}
+                >
+                  X
+                </S.Button>
+              </S.CurrencyBlock>
             )}
-          </S.Select>
-        </S.SelectBlock>
-      )}
-
-      {/* corrency converter is becomes visible only if the data from API is set */}
-      {apiData && (
-        <>
-          {display.one && (
-            <S.CurrencyBlock>
-              <S.Subitle>{apiData.bpi.USD.code}</S.Subitle>
-              <S.ColorTitle>
-                {/* the converted result is formated to currency string */}
-                {parseInt(
-                  apiData.bpi.USD.rate_float * inputData,
-                  10
-                ).toLocaleString("en-US", {
-                  style: "currency",
-                  currency: apiData.bpi.USD.code,
-                })}
-              </S.ColorTitle>
-              {/* display array spread is nessesery to update individual propery not to overwrite entire array */}
-              <S.Button onClick={() => setDisplay({ ...display, one: false })}>
-                X
-              </S.Button>
-            </S.CurrencyBlock>
-          )}
-
-          {display.two && (
-            <S.CurrencyBlock>
-              <S.Subitle>{apiData.bpi.GBP.code}</S.Subitle>
-              <S.ColorTitle>
-                {parseInt(
-                  apiData.bpi.GBP.rate_float * inputData,
-                  10
-                ).toLocaleString("en-US", {
-                  style: "currency",
-                  currency: apiData.bpi.GBP.code,
-                })}
-              </S.ColorTitle>
-              <S.Button onClick={() => setDisplay({ ...display, two: false })}>
-                X
-              </S.Button>
-            </S.CurrencyBlock>
-          )}
-
-          {display.three && (
-            <S.CurrencyBlock>
-              <S.Subitle>{apiData.bpi.EUR.code}</S.Subitle>
-              <S.ColorTitle>
-                {parseInt(
-                  apiData.bpi.EUR.rate_float * inputData,
-                  10
-                ).toLocaleString("en-US", {
-                  style: "currency",
-                  currency: apiData.bpi.EUR.code,
-                })}
-              </S.ColorTitle>
-              <S.Button
-                onClick={() => setDisplay({ ...display, three: false })}
-              >
-                X
-              </S.Button>
-            </S.CurrencyBlock>
-          )}
-        </>
-      )}
-
-      {/* if there is no API data error message is displayed */}
-      {!apiData && <div>there has been error with conversion data</div>}
+          </>
+        )
+      }
+      {
+        // if there is no API data error message is displayed
+        !apiData && <div>there has been error with conversion data</div>
+      }
     </>
   );
 }
